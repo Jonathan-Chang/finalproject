c53ab49d4997cc0a07bc7bf47d582290bd7c5eef
Eric Li <eli24@cs.binghamton.edu>
2018-11-29 13:03:44 -0500

    Completed Search function

--- src/BST.hpp
+++ src/BST.hpp
@@ -6,10 +6,14 @@
 template <typename T>
 class Node {
 public:
-	Node();
-
+	Node() {
+		data = nullptr;
+		left = nullptr;
+		right = nullptr;
+	}
+	
 private:
-	T *data;
+	T *value;
 	Node *left;
 	Node *right;
 };
@@ -20,6 +24,30 @@ public:
 	BST() {
 		root = nullptr;
 	}
+
+	void insert(T item) {
+		
+	}
+
+	void remove(T item) {
+
+	}
+
+	Node *search(T item) {
+		Node<T> *node = root;
+		
+		while (root) {
+			if (item < root->value)
+				node = node->left;
+			else if (item > root->value)
+				node = node->right;
+			else
+				return node;
+		}
+
+		return nullptr;
+	}
+
 
 private:
 	Node<T> *root;
